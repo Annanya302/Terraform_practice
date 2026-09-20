fe715657970cb52ef3cd2d58e67512146d160a0a
Annanya302 <kg9374302@gmail.com>
2026-09-20 12:23:57 +0530

    Remove file from tracking and add to .gitignore

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  #aws region comes from the selected environment's tfvars file.
-  region = var.aws_region
-
-  #terraform assumes this role to get permission to create and manage aws infrastructure resources.
-  assume_role {
-    role_arn = "arn:aws:iam::856121136436:role/TerraformRole"
-  }
-}
